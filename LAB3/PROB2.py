398a7286a12ec1376c11d363e52e43a261059eaa
Angery501 <aracely.naomi.prado@gmail.com>
2025-03-27 08:28:52 -0400

    Update PROB2.py

--- LAB3/PROB2.py
+++ LAB3/PROB2.py
@@ -1,17 +1,12 @@
 import turtle
-
-# Crear una ventana
 turtle.title("Dibuja flechas")
 turtle.speed(3)
-
-# Definir el grosor de las flechas
 grosor = 5
 turtle.pensize(grosor)
 
-# Dibuja la flecha hacia arriba (naranja, en la parte superior derecha)
 turtle.color("orange")
 turtle.penup()
-turtle.goto(50, 50)  # Posiciona la flecha
+turtle.goto(50, 50)  
 turtle.setheading(90)
 turtle.pendown()
 turtle.forward(150)
@@ -24,11 +19,10 @@ turtle.backward(20)
 turtle.right(135)
 turtle.backward(50)
 
-# Dibuja la flecha hacia la izquierda
 turtle.color("green")
 turtle.penup()
-turtle.goto(20, 210)  # Posiciona la flecha
-turtle.setheading(180)  # Apunta a la izquierda
+turtle.goto(20, 210)  
+turtle.setheading(180) 
 turtle.pendown()
 turtle.forward(120)
 turtle.right(135)
@@ -39,10 +33,10 @@ turtle.forward(20)
 turtle.backward(20)
 turtle.right(135)
 turtle.backward(50)
-# Dibuja la flecha hacia abajo (azul, en la parte inferior izquierda)
+
 turtle.color("blue")
 turtle.penup()
-turtle.goto(-120, 200)  # Posiciona la flecha
+turtle.goto(-120, 200)  
 turtle.setheading(270)
 turtle.pendown()
 turtle.forward(150)
@@ -55,21 +49,20 @@ turtle.backward(20)
 turtle.right(135)
 turtle.backward(50)
 
-# Dibuja la flecha hacia la izquierda (roja, en la parte superior izquierda)
 turtle.color("red")
 turtle.penup()
-turtle.goto(-85, 37)  # Posición de la flecha
-turtle.setheading(0)  # Apunta hacia la derecha
+turtle.goto(-85, 37)  
+turtle.setheading(0) 
 turtle.pendown()
-turtle.forward(120)  # Dibuja la línea principal de la flecha
-turtle.right(135)     # Rote hacia la izquierda para hacer la flecha
-turtle.forward(20)   # Dibuja la punta de la flecha
-turtle.backward(20)  # Regresa a la línea principal
-turtle.left(270)     # Rote hacia la izquierda
-turtle.forward(20)   # Dibuja la otra punta de la flecha
-turtle.backward(20)  # Regresa
-turtle.right(135)    # Rote hacia la derecha
-turtle.backward(50)  # Vuelve hacia atrás para finalizar
+turtle.forward(120)  
+turtle.right(135)     
+turtle.forward(20)   
+turtle.backward(20)  
+turtle.left(270)     
+turtle.forward(20)   
+turtle.backward(20)  
+turtle.right(135)    
+turtle.backward(50)  
 
-# Terminar
+
 turtle.done()
