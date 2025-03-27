63cf61c7d439ea7e2b0b548af34ff63effdf0dc8
Angery501 <aracely.naomi.prado@gmail.com>
2025-03-27 08:13:16 -0400

    Update PROB1.py

--- LAB3/PROB1.py
+++ LAB3/PROB1.py
@@ -1,33 +1,30 @@
 import turtle
 
 def dibujar_triangulo_isosceles(base, altura):
-    # Calcular la posición inicial
+    
     turtle.penup()
-    turtle.goto(-base / 2, 0)  # Mover a la posición inicial
+    turtle.goto(-base / 2, 0)  
     turtle.pendown()
 
-    # Dibujar el triángulo
-    turtle.forward(base)  # Dibujar la base
-    turtle.left(110)  # Girar 120 grados
-    turtle.forward(altura)  # Dibujar el lado izquierdo
-    turtle.left(145)  # Girar 120 grados
-    turtle.forward(altura)  # Dibujar el lado derecho
-    turtle.left(120)  # Girar 120 grados para volver a la posición inicial
+    
+    turtle.forward(base)  
+    turtle.left(110)  
+    turtle.forward(altura) 
+    turtle.left(145)  
+    turtle.forward(altura)  
+    turtle.left(120)  
 
-    turtle.done()  # Terminar el dibujo
+    turtle.done()  
 
 def main():
-    # Definir la base y la altura del triángulo
-    base = 100  # Puedes cambiar este valor
-    altura = 170  # Puedes cambiar este valor
+    
+    base = 100  
+    altura = 170  
 
-    # Configurar la tortuga
-    turtle.speed(1)  # Velocidad de dibujo
+    
+    turtle.speed(1) 
     turtle.title("Dibujo de un triángulo isósceles")
 
-    # Dibujar el triángulo isósceles
     dibujar_triangulo_isosceles(base, altura)
-
-# Ejecutar el programa
 if __name__ == "__main__":
     main()
