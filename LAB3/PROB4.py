f4b287a84374d218d944a65378778a0fdf635855
Angery501 <aracely.naomi.prado@gmail.com>
2025-03-27 08:41:23 -0400

    Update PROB4.py

--- LAB3/PROB4.py
+++ LAB3/PROB4.py
@@ -1,35 +1,30 @@
 import turtle
 
-# Configurar la ventana
-turtle.bgcolor("yellow")  # Color de fondo
+turtle.bgcolor("yellow") 
 turtle.title("Iniciales A P")
 
-# Configurar la tortuga
 t = turtle.Turtle()
-t.pensize(7)  # Grosor del trazo
-t.speed(1)  # Velocidad de dibujo
+t.pensize(7) 
+t.speed(1)  
 
-# Dibujar la letra "A"
-t.color("red")  # Color de la letra A
+t.color("red") 
 t.penup()
-t.goto(-50, 0)  # Posicionar para la letra A
+t.goto(-50, 0) 
 t.pendown()
-t.setheading(75)  # Dirigir hacia arriba
-t.forward(100)  # Lado izquierdo de A
+t.setheading(75)  
+t.forward(100)  
 t.setheading(-75) 
-t.forward(100)  # Lado derecho de A
+t.forward(100)  
 t.setheading(0)
-t.forward(50)  # Barra transversal de A
+t.forward(50) 
 
-# Dibujar la letra "P"
 t.penup()
-t.goto(10, 0)  # Posicionar para la letra P
+t.goto(10, 0) 
 t.pendown()
-t.setheading(90)  # Dirigir hacia arriba
-t.forward(100)  # Línea vertical de P
+t.setheading(90) 
+t.forward(100)  
 t.setheading(0)
-t.circle(-25, 180)  # Media circunferencia para la parte superior de P
+t.circle(-25, 180)  
 
-# Finalizar
-t.hideturtle()  # Ocultar la tortuga
-turtle.done()  # Mostrar la ventana
+t.hideturtle()  
+turtle.done()  
